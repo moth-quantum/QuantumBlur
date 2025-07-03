4c00382cc2c834739db97d298076093f59dabdb4
Astryd Park <astryd@mothquantum.com>
2025-07-03 10:23:52 +0100

    Fixed layout + Connect powerhouse

---
+++ main.js
@@ -0,0 +1,40 @@
+document.getElementById('userImage').addEventListener('change', (e) =>{
+    let files = e.target.files;
+    let preview = document.getElementById('preview');
+
+    preview.innerHTML = ''; // Clear any existing content
+
+    for (let i = 0; i < files.length; i++) {
+        let file = files[i];
+
+        if (!file.type.match('image.*')) {
+            continue;
+        }
+
+        let imgContainer = document.createElement('div');
+        // imgContainer.style.marginBottom = '20px';
+
+        let img = document.createElement('img');
+        img.src = URL.createObjectURL(file);
+
+        imgContainer.appendChild(img);
+
+        preview.appendChild(imgContainer);
+
+        console.log(`File ${i + 1}: ${file.name}, Size: ${file.size} bytes, Type: ${file.type}`);
+    }
+});
+
+qb = () => {
+    let before = document.getElementById('userImage');
+    if (before.files.length === 0) {
+        alert('Upload an image first.');
+        return;
+    }
+
+    let strengthVal = document.getElementById('strength').value;
+    console.log(`Quantum Blur strength: ${strengthVal}`);
+
+    // Call the quantum blur function from quantumblur.js
+    QuantumBlur(userImage.files[0], strengthVal);
+}
